6f98550632f4214af9cd946718b1bc4a263b8c64
Uelsky <maktavish123@gmail.com>
2024-11-02 23:20:46 +0300

    modified:   src/main/java/Game.java

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -9,7 +9,7 @@ public class Game {
 
     public Player findByName(String playerName) {
         for (Player player : players) {
-            if (player.getName() == playerName) {
+            if (player.getName().equals(playerName)) {
                 return player;
             }
         }
